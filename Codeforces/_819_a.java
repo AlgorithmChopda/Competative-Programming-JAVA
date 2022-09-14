09145b015f7c45e00e510d7da5e73e9540524367
AlgorithmChopda <vinaychopda4@gmail.com>
2022-09-14 11:49:13 +0530

    Create _819_a.java

---
+++ Codeforces/_819_a.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codeforces;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vinay Jain
+ */
+public class _819_a {
+    static class Solution {
+        public void solve(Scanner sc){
+            int n = sc.nextInt();
+            int ar[] = new int[n];
+            for(int i = 0 ; i < n ; i++){
+                ar[i] = sc.nextInt();
+                
+            }
+                    
+                    
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-- > 0)
+            new Solution().solve(sc);
+    }
+}
